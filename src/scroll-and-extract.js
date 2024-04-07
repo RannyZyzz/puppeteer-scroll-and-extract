f60c980038f8fd7a7f6b658c82b18076b5842c96
ranny-pop-os-22-04 <rannierreis@gmail.com>
2024-04-07 19:22:20 -0300

    realizando testes para raspagem de comentários

--- src/scroll-and-extract.js
+++ src/scroll-and-extract.js
@@ -1,5 +1,5 @@
 import puppeteer from 'puppeteer';
-import fs from 'fs';
+
 
 async function scrollAndExtract() {
     const browser = await puppeteer.launch({ headless: false });
@@ -7,43 +7,60 @@ async function scrollAndExtract() {
 
     await page.setViewport({ width: 1080, height: 1024 });
 
-    await page.goto('https://www.olx.com.br/estado-sc/florianopolis-e-regiao?q=macbook%20m1', { waitUntil: 'networkidle2' });
+    await page.goto('https://play.google.com/store/apps/details?id=br.com.mobilesaude.unimed.valedosinos&hl=pt_BR&gl=US', { waitUntil: 'networkidle2' });
 
-    let elements = [];
+    await page.evaluate(() => {
+        window.scrollBy(0, window.innerHeight);
+        window.scrollBy(0, window.innerHeight);
+    });
 
-    async function extractInnerText() {
-        const extractedElements = await page.$$eval('.iWOuRR', elements => {
-            return elements.map(element => element.innerText);
-        });
-        elements = elements.concat(extractedElements);
-    }
+    await page.keyboard.press('Enter');
 
-    async function scrollPage() {
-        await page.evaluate(() => {
-            window.scrollBy(0, window.innerHeight);
-        });
-    }
+    await page.click('#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div:nth-child(1) > div > div.wkMJlb.YWi3ub > div > div.qZmL0 > div:nth-child(1) > c-wiz:nth-child(5) > section > div > div.Jwxk6d > div:nth-child(5) > div > div > button > span');
 
-    // Defina o intervalo de tempo em milissegundos para rolar a página
-    const scrollInterval = 1000; // Rolar a página a cada 2 segundos
+    await page.focus('div.jgIq1')
 
-    // Rolar a página periodicamente
-    const scrollIntervalId = setInterval(async () => {
-        await scrollPage();
-        await extractInnerText();
-        //incluir lógica para breakar o código quando chegar ao fim da pagina
-    }, scrollInterval);
+    await page.evaluate(() => {
+        const modal = document.querySelector('div.jgIq1');
+        modal.scrollTo(0, modal.scrollHeight); // Rolar para o final do modal
+        // Você também pode ajustar o valor de scrollTo conforme necessário
+    });
 
-    // Aguarde um tempo suficiente para rolar a página e coletar os elementos
-    await new Promise(resolve => setTimeout(resolve, 30000));
+    // let elements = []; 
+
+    // async function extractInnerText() {
+    //     const extractedElements = await page.$$eval('.VfPpkd-vQzf8d', elements => {
+    //         return elements.map(element => element.innerText);
+    //     });
+    //     elements = elements.concat(extractedElements);
+    // }
+
+    // async function scrollPage() {
+    //     await page.evaluate(() => {
+    //         window.scrollBy(0, window.innerHeight);
+    //     });
+    // }
+
+    // // Defina o intervalo de tempo em milissegundos para rolar a página
+    // const scrollInterval = 1000; // Rolar a página a cada 2 segundos
+
+    // // Rolar a página periodicamente
+    // const scrollIntervalId = setInterval(async () => {
+    //     await scrollPage();
+    //     await extractInnerText();
+    //     //incluir lógica para breakar o código quando chegar ao fim da pagina
+    // }, scrollInterval);
+
+    // // Aguarde um tempo suficiente para rolar a página e coletar os elementos
+    // await new Promise(resolve => setTimeout(resolve, 30000));
 
 
-    clearInterval(scrollIntervalId);
+    // clearInterval(scrollIntervalId);
 
-    fs.writeFileSync('resultado.txt', elements.join('\n'), 'utf-8');
-    console.log("Resultados salvos em 'resultados.txt'");
+    // fs.writeFileSync('resultado.txt', elements.join('\n'), 'utf-8');
+    // console.log("Resultados salvos em 'resultados.txt'");
 
-    await browser.close();
+    // await browser.close();
 }
 
 scrollAndExtract();
