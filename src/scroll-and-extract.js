ac75d2db4865ca75315995b61f1f6cf2f3c7d887
ranny-pop-os-22-04 <rannierreis@gmail.com>
2024-04-08 21:48:37 -0300

    implementa v1 de scroll e extract dados de lojas pré determinadas

--- src/scroll-and-extract.js
+++ src/scroll-and-extract.js
@@ -1,101 +1,134 @@
 import puppeteer from 'puppeteer';
 import fs from 'fs';
-import { createObjectCsvWriter } from 'csv-writer';
+import path from 'path';
+import { promisify } from 'util';
+import { urlStores } from './urlStores.js';
 
-async function scrollAndExtract() {
+// process.setMaxListeners(15);
+const mkdirAsync = promisify(fs.mkdir);
+const rmdirAsync = promisify(fs.rmdir);
+
+async function scrollAndExtract(urlLoja, folderCliente) {
+
+    const __dirname = path.dirname(new URL(import.meta.url).pathname);
+    const resenhasFolderPath = path.join(__dirname, 'resenhas');
+
+    try {
+        await mkdirAsync(resenhasFolderPath);
+    } catch (err) {
+        if (err.code !== 'EEXIST') {
+            throw err;
+        }
+    }
+
+    const folderName = folderCliente;
+    const folderPath = path.join(resenhasFolderPath, folderName);
+
+    try {
+        await mkdirAsync(folderPath);
+    } catch (err) {
+        if (err.code !== 'EEXIST') {
+            throw err;
+        }
+    }
+
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
-    await page.setViewport({ width: 1080, height: 1024 });
+    try {
+        await page.setViewport({ width: 1080, height: 1024 });
+        await page.goto(`${urlLoja}`, { waitUntil: 'networkidle2' });
+        await page.evaluate(() => {
+            window.scrollBy(0, window.innerHeight);
+            window.scrollBy(0, window.innerHeight);
+        });
+        await page.keyboard.press('Enter');
+        await page.click('#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div:nth-child(1) > div > div.wkMJlb.YWi3ub > div > div.qZmL0 > div:nth-child(1) > c-wiz:nth-child(5) > section > div > div.Jwxk6d > div:nth-child(5) > div > div > button > span');
+        await page.focus('div.jgIq1')
+        await page.waitForSelector('#yDmH0d > div.VfPpkd-Sx9Kwc.cC1eCc.UDxLd.PzCPDd.HQdjr.VfPpkd-Sx9Kwc-OWXEXe-FNFY6c > div.VfPpkd-wzTsW > div > div > div > div > div.fysCi');
+        async function scrollForDuration(page, duration) {
+            const endTime = Date.now() + duration;
+            while (Date.now() < endTime) {
+                await page.evaluate(() => {
+                    const modal = document.querySelector('#yDmH0d > div.VfPpkd-Sx9Kwc.cC1eCc.UDxLd.PzCPDd.HQdjr.VfPpkd-Sx9Kwc-OWXEXe-FNFY6c > div.VfPpkd-wzTsW > div > div > div > div > div.fysCi');
+                    modal.scrollTo(0, modal.scrollHeight);
+                });
+                await new Promise(resolve => setTimeout(resolve, 1000)); // Aguarda 1 segundo
+            }
+        }
 
-    await page.goto('https://play.google.com/store/apps/details?id=br.com.mobilesaude.unimed.valedosinos&hl=pt_BR&gl=US', { waitUntil: 'networkidle2' });
 
-    await page.evaluate(() => {
-        window.scrollBy(0, window.innerHeight);
-        window.scrollBy(0, window.innerHeight);
-    });
+        await scrollForDuration(page, 10000); // Scroll por 10 segundos
+        const data = await page.$$eval('span.bp9Aid, .h3YV2d, div.iXRFPc', divs => {
+            return divs.map(div => {
+                if (div.classList.contains('bp9Aid')) {
+                    return { type: 'data', value: div.innerText.trim() };
+                } else if (div.classList.contains('h3YV2d')) {
+                    console.log(div.innerText.trim())
+                    return { type: 'comentario', value: div.innerText.trim() };
+                } else if (div.classList.contains('iXRFPc')) {
+                    return { type: 'ariaLabel', value: div.getAttribute('aria-label').trim() };
+                }
+            }).filter(item => item !== undefined);
+        });
+        await browser.close();
 
-    await page.keyboard.press('Enter');
+        // Organizando os dados em pares de "data" e "comentário"
+        const pairs = [];
+        let currentData;
+        let currentComentario;
+        data.forEach(item => {
+            if (item.type === 'data') {
+                currentData = item.value;
+            } else if (item.type === 'comentario') {
+                currentComentario = item.value;
+            } else if (item.type === 'ariaLabel') {
+                const pair = {
+                    data: currentData,
+                    comentario: currentComentario,
+                    ariaLabel: item.value
+                };
+                pairs.push(pair);
+            }
+        });
 
-    await page.click('#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div:nth-child(1) > div > div.wkMJlb.YWi3ub > div > div.qZmL0 > div:nth-child(1) > c-wiz:nth-child(5) > section > div > div.Jwxk6d > div:nth-child(5) > div > div > button > span');
+        const csvData = pairs.map(pair => `"${pair.data}";"${pair.comentario}";"${pair.ariaLabel}"`).join('\n');
+        fs.writeFileSync(`${folderPath}/dados-${folderCliente}.csv`, '"Data";"Comentário";"Avaliação"\n' + csvData);
 
-    await page.focus('div.jgIq1')
-
-
-    await page.waitForSelector('#yDmH0d > div.VfPpkd-Sx9Kwc.cC1eCc.UDxLd.PzCPDd.HQdjr.VfPpkd-Sx9Kwc-OWXEXe-FNFY6c > div.VfPpkd-wzTsW > div > div > div > div > div.fysCi');
-
-    async function scrollForDuration(page, duration) {
-        const endTime = Date.now() + duration;
-        while (Date.now() < endTime) {
-            await page.evaluate(() => {
-                const modal = document.querySelector('#yDmH0d > div.VfPpkd-Sx9Kwc.cC1eCc.UDxLd.PzCPDd.HQdjr.VfPpkd-Sx9Kwc-OWXEXe-FNFY6c > div.VfPpkd-wzTsW > div > div > div > div > div.fysCi');
-                modal.scrollTo(0, modal.scrollHeight);
-            });
-            await new Promise(resolve => setTimeout(resolve, 1000)); // Aguarda 1 segundo
+        console.log('Dados foram escritos no arquivo dados.csv');
+    } catch (err) {
+        console.error(`${folderCliente} : Erro ao salvar a resenha:`, err);
+        // Se ocorrer um erro, exclui o diretório folderCliente
+        try {
+            await rmdirAsync(folderPath, { recursive: true });
+            console.log(`Diretório ${folderCliente} excluído.`);
+        } catch (removeErr) {
+            console.error(`Erro ao excluir o diretório ${folderCliente}:`, removeErr);
         }
     }
-    await scrollForDuration(page, 10000); // Scroll por 10 segundos
-
-    const data = await page.$$eval('span.bp9Aid, .h3YV2d', divs => {
-        return divs.map(div => div.innerText.trim());
-    });
-
-
-    await browser.close();
-
-    const csvWriter = createObjectCsvWriter({
-        path: 'dados.csv',
-        header: [
-            { id: 'data', title: 'Data' },
-            { id: 'informacao', title: 'Informação' }
-        ]
-    });
-
-    const records = data.map(info => {
-        console.log(info)
-        const [data, informacao] = info.split('\n')
-        return { data, informacao };
-    });
-    csvWriter.writeRecords(records)
-        .then(() => console.log('Dados salvos com sucesso em dados.csv'))
-        .catch(error => console.error('Erro ao salvar os dados:', error));
-
-
-    // let elements = []; 
-
-    // async function extractInnerText() {
-    //     const extractedElements = await page.$$eval('.VfPpkd-vQzf8d', elements => {
-    //         return elements.map(element => element.innerText);
-    //     });
-    //     elements = elements.concat(extractedElements);
-    // }
-
-    // async function scrollPage() {
-    //     await page.evaluate(() => {
-    //         window.scrollBy(0, window.innerHeight);
-    //     });
-    // }
-
-    // // Defina o intervalo de tempo em milissegundos para rolar a página
-    // const scrollInterval = 1000; // Rolar a página a cada 2 segundos
-
-    // // Rolar a página periodicamente
-    // const scrollIntervalId = setInterval(async () => {
-    //     await scrollPage();
-    //     await extractInnerText();
-    //     //incluir lógica para breakar o código quando chegar ao fim da pagina
-    // }, scrollInterval);
-
-    // // Aguarde um tempo suficiente para rolar a página e coletar os elementos
-    // await new Promise(resolve => setTimeout(resolve, 30000));
-
-
-    // clearInterval(scrollIntervalId);
-
-    // fs.writeFileSync('resultado.txt', elements.join('\n'), 'utf-8');
-    // console.log("Resultados salvos em 'resultados.txt'");
-
-    // await browser.close();
 }
 
-scrollAndExtract();
+async function processURLs(urls) {
+    for (const url of urls) {
+        const urlLoja = url[1];
+        const folderCliente = url[0];
+
+        try {
+            await scrollAndExtract(urlLoja, folderCliente);
+            console.log(`${folderCliente} : Resenha salva com sucesso!`);
+        } catch (err) {
+            console.error(`${folderCliente} : Erro ao salvar a resenha:`, err);
+        }
+    }
+}
+
+// Limita o número de instâncias do navegador abertas simultaneamente
+const CONCURRENT_INSTANCES = 2;
+const chunks = [];
+for (let i = 0; i < urlStores.length; i += CONCURRENT_INSTANCES) {
+    chunks.push(urlStores.slice(i, i + CONCURRENT_INSTANCES));
+}
+
+for (const chunk of chunks) {
+    await processURLs(chunk);
+}
